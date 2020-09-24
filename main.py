ac45ae650531607d17ad50d2e1bdfea64e39c2fc
victoriamatush <victoria603111@gmail.com>
2020-09-24 08:07:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-a = int(input("Enter a number >=1 and <=109: "))
-for i in range(a+1):
-    if(i**i % a == 0 ):
-        print(i)
+def function(k):
+    for i in range(k + 1):
+        if (i ** i % k == 0):
+            print(i)
+            break
+
+
+while True:
+    try:
+        a = int(input("Enter a number >=1 and <=109: "))
         break
+    except ValueError:
+        print('Please enter a whole number')
+
+function(a)
