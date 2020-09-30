c4fd8164a0ef572721c3981172acf158ce2f319d
victoriamatush <victoria603111@gmail.com>
2020-09-30 14:25:39 +0300

    Second changes for task 2 of programming

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from random import randint
 
 
-def indexsort(s, p):
-    result = list()
-    for i in range(p):
+def indexsort(arr, size):
+    for i in range(size):
         if i % 2 == 0:
-            result.append(s[i])
-    for i in range(p):
+            arr.append(arr[i])
+    for i in range(size):
         if i % 2 == 1:
-            result.append(s[i])
-    return result
+            arr.append(arr[i])
+    del arr[0:size]
+    return arr
 
 
 def motion(f, step):
@@ -28,44 +28,45 @@ def main():
     while True:
         try:
             choice = int(input("Random numbers - press 1, enter by yourself - press 2, want to end program - 3: "))
-            size = int(input("Enter amount of numbers: "))
-            break
         except ValueError:
             print('Please enter a whole number')
 
 
-    if choice == 1:
+        if choice == 1:
+            while True:
+                try:
+                    size = int(input("Enter amount of numbers: "))
+                    a = int(input("Enter low limit: "))
+                    b = int(input("Enter high limit: "))
+                    break
+                except ValueError:
+                    print('Please enter a whole number')
+            for i in range(int(size)):
+                array.append(randint(a, b))
+        if choice == 2:
+            size = int(input("Enter amount of numbers: "))
+            print("Enter the elements of the array:")
+            for i in range(int(size)):
+                array.append(int(input("Element " + str(i) + ": ")))
+
+        if choice == 3:
+            break
+
+        print(array)
+        array = indexsort(array, size)
+        print("Array sorted by indexes: ")
+        print(array)
+
         while True:
             try:
-                a = int(input("Enter low limit: "))
-                b = int(input("Enter high limit: "))
+                k = int(input("Enter amount of positions to shift: "))
                 break
             except ValueError:
                 print('Please enter a whole number')
-        for i in range(int(size)):
-            array.append(randint(a, b))
-    if choice == 2:
-        print("Enter the elements of the array:")
-        for i in range(int(size)):
-            array.append(int(input("Element " + str(i) + ": ")))
 
-
-    print(array)
-
-    array = indexsort(array, size)
-    print("Array sorted by indexes: ")
-    print(array)
-
-    while True:
-        try:
-            k = int(input("Enter amount of positions to shift: "))
-            break
-        except ValueError:
-            print('Please enter a whole number')
-
-    motion(array, k)
-    print(array)
+        motion(array, k)
+        print(array)
+        array.clear()
 
 
 main()
-
