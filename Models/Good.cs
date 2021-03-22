9294898576ba6bcd72af2b879103ab4baac3843c
victoriamatush <victoria603111@gmail.com>
2021-03-22 17:47:55 +0200

    REST API, task 3 for practics

---
+++ Models/Good.cs
@@ -0,0 +1,39 @@
+﻿using RESTAPI.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPI.Models
+{
+    public class Good
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]{3}[-][0-9]{3}[-][0-9]{3}$")]
+        public string Code { get; set; }
+
+        [Required]
+        [TitleValidation]
+        public string Title { get; set; }
+
+        [Required]
+        [TypeValidation]
+        public string Type { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Amount { get; set; }
+
+        [Required]
+        [PriceValidation]
+        public double Price { get; set; }
+
+        [Required]
+        [TitleValidation]
+        public string Data { get; set; }
+    }
+}
