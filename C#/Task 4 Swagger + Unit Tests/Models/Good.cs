b954342df6c300e93de711dae1d5d00ea8ac30de
victoriamatush <victoria603111@gmail.com>
2021-04-05 15:21:46 +0300

    Task 4 RESTAPI Swagger Unit Tests

---
+++ C#/Task 4 Swagger + Unit Tests/Models/Good.cs
@@ -0,0 +1,39 @@
+﻿using RESTAPI.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPI.Models
+{
+    public class Good
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Code bad format")]
+        [RegularExpression("^[0-9]{3}[-][0-9]{3}[-][0-9]{3}$")]
+        public string Code { get; set; }
+
+        [Required]
+        [TitleValidation(ErrorMessage = "Only letters")]
+        public string Title { get; set; }
+
+        [Required]
+        [TypeValidation(ErrorMessage = "Only box or letter")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Not int")]
+        [Range(1, int.MaxValue)]
+        public int Amount { get; set; }
+
+        [Required]
+        [PriceValidation(ErrorMessage = "Price should be > 0 ")]
+        public double Price { get; set; }
+
+        [Required]
+        [TitleValidation(ErrorMessage = "Only letters")]
+        public string Data { get; set; }
+    }
+}
---
+++ C#/Task 4 Swagger + Unit Tests/Models/GoodContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPI.Models
+{
+    public class GoodContext : DbContext
+    {
+        public GoodContext() { }
+        public GoodContext(DbContextOptions<GoodContext> options)
+            : base(options) { }
+
+        public DbSet<Good> Goods { get; set; }
+    }
+}
